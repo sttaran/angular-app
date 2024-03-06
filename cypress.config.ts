175a4640c7968e2d55f8c57901b30649c1a7fc3a
Vlad <shostvladyslav@gmail.com>
2024-03-06 23:21:34 +0200

    update cypress.config.ts add viewport

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,8 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:56299',
     specPattern: 'cypress/e2e/**/*.test.{js,jsx}',
+    viewportHeight: 1080,
+    viewportWidth: 1920,
     requestTimeout: 5000,
     responseTimeout: 30000,
     screenshotOnRunFailure: true,
