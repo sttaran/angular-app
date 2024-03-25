bda100700818948d5279bfe777db07dfd5e56da5
Narine <narinegasparian1@gmail.com>
2024-03-25 16:00:11 +0000

    commit task1.cy.js task2.cy.js task3.cy.js

---
+++ cypress/e2e/hwcypress/task2.cy.js
@@ -0,0 +1,13 @@
+describe('modal-overlays', () => {
+  it('Click Button and Verify Modal Dialog', () => {
+    const containerSelector = 'ngx-modal-overlays nb-card';
+
+    cy.visit('http://localhost:4200/pages/modal-overlays/dialog');
+
+    cy.contains('.appearance-filled.size-medium.shape-rectangle.status-basic.nb-transition', 'Enter Name').click();
+
+    cy.get('button[status="success"].appearance-filled.size-medium.shape-rectangle.nb-transition')
+      .should('exist')
+      .and('be.visible');
+  });
+});
---
+++ cypress/e2e/hwcypress/task3.cy.js
@@ -0,0 +1,17 @@
+describe('modal-overlays', () => {
+  it('Click Button and Verify Modal Dialog Content', () => {
+    cy.visit('http://localhost:4200/pages/modal-overlays/dialog');
+
+    cy.contains('.appearance-filled.size-medium.shape-rectangle.status-basic.nb-transition', 'Enter Name').click();
+    cy.get('input[placeholder="Name"].size-medium.status-basic.shape-rectangle.nb-transition')
+      .should('exist');
+    cy.get('nb-card-header').should('exist').and('contain', 'Enter your name');
+    cy.get('button[status="success"].appearance-filled.size-medium.shape-rectangle.nb-transition')
+      .should('exist')
+      .and('contain', 'Submit');
+    cy.get('button[status="danger"].appearance-filled.size-medium.shape-rectangle.nb-transition')
+      .should('exist')
+      .and('have.text', 'Cancel');
+
+  });
+});
